034ed0be2dd4306c1420ecc429330e98aa6c565f
Naidu <Naidu@192.168.100.6>
2019-08-06 22:28:15 +0530

    added to Sample.java

--- TestProject/src/sample.java
+++ TestProject/src/sample.java
@@ -2,6 +2,10 @@
 public class sample {
 	public static void main(String[] args) {
 		System.out.println("sdfkgbdg");
+		System.out.println("sdfkgbdg");
+		System.out.println("sdfkgbdg");
 	}
+	
+	
 
 }
